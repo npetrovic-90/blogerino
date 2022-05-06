29ee54dcf8f09c62e8fa23266625b2923c692d78
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2022-05-06 15:19:47 +0200

    Footer design.

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,93 @@
+<footer class="bg-gray-800 py-20 mt-20">
+    <div class="sm:grid grid:cols-3 w-25 pb-10 m-auto border-b-2 border-gray-700">
+        <div>
+            <h3 class="text-l sm:font-bold">
+                Pages
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Home
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/blog">
+                        Blog
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/login">
+                        Login
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/register">
+                        Register
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold">
+                Find Us
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        What we do
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Address
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Phone
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold">
+                Latest posts
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Why we love tech
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Why we love design
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Games of the year
+                    </a>
+                </li>
+                <li class="pb-1">
+                    <a href="/">
+                        Why to use PHP
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
+        Copyright 2010-2022 All Rights Reserved
+    </p>
+</footer>
