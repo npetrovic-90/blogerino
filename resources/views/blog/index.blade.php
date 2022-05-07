b19f25524a0ffaf67bec020b30d93754caaf7c0d
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2022-05-07 10:54:53 +0200

    Storing a post.

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,10 +9,29 @@
             </h1>
         </div>
     </div>
+
+    @if(session()->has('message'))
+        <div class="w-4/5 m-auto mt-10 pl-2">
+            <p class="w-1/6 mb-4 text-gray-50 bg-green-500 py-4 rounded-2xl px-4">
+                {{ session()->get('message') }}
+            </p>
+        </div>
+    @endif
+
+    @if(Auth::check())
+        <div class="pt-15 w-4/5 m-auto">
+        <a href="{{ URL('/blog/create') }}"
+           class=" bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+            Create post
+
+        </a>
+        </div>
+    @endif
+
     @foreach($posts as $post)
     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
         <div>
-            <img src="https://pixabay.com/get/gc973a77547db6d2c12c8b3577463c08e698293057ebebc99cdf5535c8065b7eccf9269600eea95c0617adc316e18abdb658b41c86952a59cda90c14980b0295ccb3d60eed86ff2069dbab963f9598a6c_1920.jpg"  alt="">
+            <img src="{{ URL('images/'.$post->image_path) }}"  alt="">
         </div>
         <div>
             <h2 class="text-gray-700 font-bold text-5xl pb-4">
